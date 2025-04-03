c5bb0dfa797ee42873b0a10b3f366808c1cd5ebd
Khushi Rani <151911119+KhushiRani005@users.noreply.github.com>
2025-04-03 19:37:59 +0530

    Add files via upload
    Upload new files

--- main.js
+++ main.js
@@ -2,5 +2,35 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () =>{
        search.classList.toggle('active');
-
+       menu.classList.remove('active');
 }
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+       menu.classList.toggle('active');
+       search.classList.remove('active');
+}
+//Hide Menu and Search Box on Scroll
+window.onscroll =() => {
+       menu.classList.remove('active');
+       search.classList.remove('active');
+}
+
+//Header
+let header = document.querySelector('search-box');
+
+window.addEventListener('scroll', () => {
+       header.classList.toggle('shadow', window.scrolly >0);     
+});
+
+//home page
+const loginmessage =localStorage.getItem('loginStatus');
+
+if(loginmessage){
+       document.getElementById('loginmessage').innerHTML=loginmessage;
+}
+
+setTimeout(function(){
+       localStorage.removeItem('loginStatus');
+       document.getElementById('loginmessage').innerHTML="Login";
+},60000);
